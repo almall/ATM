0a27fa49e7c98ff4ecf9bedb34b6d89e854d94df
Alejandro <alejandromartilledo1995@gmail.com>
2023-02-09 00:07:29 +0100

    rev code 3

--- ATM.py
+++ ATM.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-balance_i = int(input("Dime la cantidad inicial: "))
+balance_i = input(float("Dime la cantidad inicial: "))
 
 print("  ATM ")
 print("\n")
@@ -8,7 +8,8 @@ print("""
 1)   Balance
 2)   Retirada
 3)   Depósito
-4)   Salir
+4)   Exportar a CSV y salir
+5)   Salir
 """)
 
 df = pd.DataFrame(columns=['Transaccion', 'Balance'])
@@ -32,26 +33,28 @@ def salir():
 conteo = 0
 
 while True:
-    opcion = int(input("Elige una de las 4 opciones: "))
+        opcion = input(float("Elige una de las 5 opciones: "))
 
-    if opcion == 1:
-        conteo = conteo + 1
-        balance_i = bal(balance_i)
-        df = df.append({'Transaccion': conteo, 'Balance':bal(balance_i)}, ignore_index=True)
-        print(df)
-    elif opcion == 2:
-        retiro = int(input("Ingrese la cantidad a retirar: "))
-        balance_i = retirada(balance_i,retiro)
-        conteo = conteo + 1
-        df = df.append({'Transaccion': conteo, 'Balance':balance_i}, ignore_index=True)
-        print(df)
-    elif opcion == 3:
-        ingreso = int(input("Ingrese la cantidad a depositar: "))
-        balance_i = deposito(balance_i, ingreso)
-        conteo = conteo + 1
-        df = df.append({'Transaccion': conteo, 'Balance':balance_i}, ignore_index=True)
-        print(df)
-    elif opcion == 4:
-        exit()
-    else:
-        break
+        if opcion == 1:
+            conteo = conteo + 1
+            balance_i = bal(balance_i)
+            df = df.append({'Transaccion': conteo, 'Balance':bal(balance_i)}, ignore_index=True)
+            print(df)
+        elif opcion == 2:
+            retiro = input(float("Ingrese la cantidad a retirar: "))
+            balance_i = retirada(balance_i,retiro)
+            conteo = conteo + 1
+            df = df.append({'Transaccion': conteo, 'Balance':balance_i}, ignore_index=True)
+            print(df)
+        elif opcion == 3:
+            ingreso = input(float("Ingrese la cantidad a depositar: "))
+            balance_i = deposito(balance_i, ingreso)
+            conteo = conteo + 1
+            df = df.append({'Transaccion': conteo, 'Balance':balance_i}, ignore_index=True)
+            print(df)
+        elif opcion == 4:
+            df.to_csv('ATM.csv',index = False)
+        elif opcion == 5:
+            exit()
+        else:
+            break
