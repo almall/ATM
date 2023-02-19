f375204e4b5213bc3cb5611d49b02aa3ff03c84f
Alejandro <alejandromartilledo1995@gmail.com>
2023-02-19 16:44:17 +0100

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -20,7 +20,7 @@ print("""
 13)  Salir
 """)
 
-df = pd.DataFrame(columns=['Transacción', 'Tipo', 'Balance'])
+df = pd.DataFrame(columns=['Transacción', 'Tipo', 'Balance', 'Transacción'])
 
 print(df)
 
@@ -95,67 +95,67 @@ while True:
         if opcion == 1:
             conteo = conteo + 1
             balance_i = bal(balance_i)
-            df = df.append({'Transacción': conteo, 'Tipo': ("Balance"), 'Balance':bal(balance_i)}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("Balance"), 'Balance':bal(balance_i), 'Transacción' : '0'}, ignore_index=True)
             print(df)
         elif opcion == 2:
             retiro = float(input("Cantidad a retirar: "))
             balance_i = retirada(balance_i, retiro)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("Retiro"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("Retiro"), 'Balance':balance_i, 'Transacción' : -retiro}, ignore_index=True)
             print(df)
         elif opcion == 3:
             ingreso = float(input("Cantidad a depositar: "))
             balance_i = deposito(balance_i, ingreso)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("Depósito"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("Depósito"), 'Balance':balance_i, 'Transacción' : ingreso}, ignore_index=True)
             print(df)
         elif opcion == 4:
             biz = float(input("Cobro BIZUM: "))
             balance_i = bizum_charge(balance_i, biz)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("BIZUM +"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("BIZUM +"), 'Balance':balance_i, 'Transacción' : biz}, ignore_index=True)
             print(df)
         elif opcion == 5:
             biz = float(input("Pago BIZUM: "))
             balance_i = bizum_pay(balance_i, biz)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("BIZUM -"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("BIZUM -"), 'Balance':balance_i, 'Transacción' : -biz}, ignore_index=True)
             print(df)
         elif opcion == 6:
             transfer = float(input("Transferencia externa: "))
             balance_i = transfer_ex_charge(balance_i, transfer)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("Transferencia externa +"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("Transferencia externa +"), 'Balance':balance_i, 'Transacción' : transfer}, ignore_index=True)
             print(df)
         elif opcion == 7:
             transfer = float(input("Transferencia externa: "))
             balance_i = transfer_ex_pay(balance_i, transfer)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("Transferencia externa -"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("Transferencia externa -"), 'Balance':balance_i, 'Transacción' : -transfer}, ignore_index=True)
             print(df)
         elif opcion == 8:
             transfer = float(input("Transferencia entre cuentas: "))
             balance_i = transfer_cu_charge(balance_i, transfer)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("Transferencia entre cuentas +"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("Transferencia entre cuentas +"), 'Balance':balance_i, 'Transacción' : transfer}, ignore_index=True)
             print(df)
         elif opcion == 9:
             transfer = float(input("Transferencia entre cuentas: "))
             balance_i = transfer_cu_pay(balance_i, transfer)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("Transferencia entre cuentas -"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("Transferencia entre cuentas -"), 'Balance':balance_i, 'Transacción' : -transfer}, ignore_index=True)
             print(df)
         elif opcion == 10:
             recibo = float(input("Cuantía del recibo a pagar: "))
             balance_i = recib(balance_i, recibo)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("Recibo"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("Recibo"), 'Balance':balance_i, 'Transacción' : -recibo}, ignore_index=True)
             print(df)
         elif opcion == 11:
             tarj = float(input("Cargo con tarjeta: "))
             balance_i = tarjeta(balance_i, tarj)
             conteo = conteo + 1
-            df = df.append({'Transacción': conteo, 'Tipo': ("Pago con tarjeta"), 'Balance':balance_i}, ignore_index=True)
+            df = df.append({'Transacción': conteo, 'Tipo': ("Pago con tarjeta"), 'Balance':balance_i, 'Transacción' : -tarj}, ignore_index=True)
             print(df)
         elif opcion == 12:
             df.to_csv('ATM.csv',mode = 'a', index = False)
