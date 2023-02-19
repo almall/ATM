28f93da1b37ed7426c53c3442ddc289bf201b2a7
Alejandro <alejandromartilledo1995@gmail.com>
2023-02-19 16:25:48 +0100

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -4,21 +4,6 @@ balance_i = float(input("Dime la cantidad inicial: "))
 
 print(" ATM ")
 print("\n")
-print("""
-1)   Balance
-2)   Retirada
-3)   Depósito
-4)   Ingreso mediante BIZUM +
-5)   Pago mediante BIZUM -
-6)   Ingreso mediante transferencia externa +
-7)   Pago mediante transferencia externa -
-8)   Ingreso mediante transferencia entre cuentas +
-9)   Pago mediante transferencia entre cuentas -
-10)  Recibo bancario
-11)  Pago con tarjeta
-12)  Exportar a CSV y salir
-13)  Salir
-""")
 
 df = pd.DataFrame(columns=['Transacción', 'Tipo', 'Balance'])
 
